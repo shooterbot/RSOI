92b1e0fa771269117c7b9f4f59c2a5bb9a21d4df
Dmintriy Ivanov <shooterbot@gmail.com>
2022-08-19 01:58:38 +0300

    Started recommendations handler

---
+++ src/recommendations/handlers/recommendation.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"RSOI/src/recommendations/models"
+	"RSOI/src/recommendations/usecases"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type RecommendationsHandlers struct {
+	rc usecases.IRecommendationsUsecase
+}
+
+func NewRecommendationsHandlers(recommendationsCase usecases.IRecommendationsUsecase) *RecommendationsHandlers {
+	return &RecommendationsHandlers{rc: recommendationsCase}
+}
+
+func writeError(w http.ResponseWriter, err string, status int) {
+	w.WriteHeader(status)
+	_, _ = w.Write([]byte(fmt.Sprintf(`{"message":"%s"}`, err)))
+}
+
+func (rh *RecommendationsHandlers) GetRecommendations(w http.ResponseWriter, r *http.Request) {
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("Failed to close response body")
+		}
+	}(r.Body)
+
+	prefs := &models.PreferencesList{}
+	err := json.NewDecoder(r.Body).Decode(prefs)
+	if err != nil {
+		fmt.Println("Failed to decode the received json")
+		writeError(w, "Bad json given as input", http.StatusBadRequest)
+		return
+	}
+
+	rec := rh.rc.GetRecommendations(prefs)
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(rec)
+	if err != nil {
+		fmt.Println("Encoding json error: ", err)
+		http.Error(w, "Failed to encode data to json", http.StatusInternalServerError)
+		return
+	}
+}
