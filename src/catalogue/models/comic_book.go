3e254a838802a8d01b5332cc1051d840b8e5e4f8
Dmintriy Ivanov <shooterbot@gmail.com>
2022-08-19 17:04:43 +0300

    Upgraded comic book model

--- src/catalogue/models/comic_book.go
+++ src/catalogue/models/comic_book.go
@@ -1,8 +1,12 @@
 package models
 
 type Book struct {
-	Id        int    `json:"id"`
-	Uuid      string `json:"Uuid"`
-	Name      string `json:"name"`
-	Publisher string `json:"publisher"`
+	Id        int      `json:"id"`
+	Uuid      string   `json:"Uuid"`
+	Name      string   `json:"name"`
+	Publisher string   `json:"publisher"`
+	Year      int      `json:"year"`
+	Rating    int      `json:"rating"`
+	Status    bool     `json:"status"`
+	Tags      []string `json:"tags"`
 }
--- src/recommendations/models/recommendations.go
+++ src/recommendations/models/recommendations.go
@@ -4,3 +4,14 @@ type PreferencesList struct {
 	Likes    []int
 	Dislikes []int
 }
+
+type Book struct {
+	Id        int
+	Uuid      string
+	Name      string
+	Publisher string
+	Year      int
+	Rating    int
+	Status    bool
+	Tags      []string
+}
