0b4e61697185fbfbde4d655c9f6f5b88fb178876
Dmitriy Ivanov <shooterbot@gmail.com>
2022-08-27 15:14:38 +0300

    Added Get Catalogue and Get Recommendations to gateway connector

--- src/gateway/connector/connector_implementation/connector.go
+++ src/gateway/connector/connector_implementation/connector.go
@@ -3,6 +3,7 @@ package connector_implementation
 import (
 	"RSOI/src/gateway/connector"
 	"RSOI/src/gateway/models"
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -42,4 +43,65 @@ func (gc *GatewayConnector) GetUserPreferences(userUuid string) (*models.Prefere
 		return nil, err
 	}
 	return res, nil
+}
+
+func (gc *GatewayConnector) GetCatalogue() (*[]models.Book, error) {
+	url := fmt.Sprintf(gc.config.CatalogueAddress + gc.config.ApiPath)
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Failed to get comics from internal service")
+		err = errors.New("Failed to get comics from internal service")
+		return nil, err
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("Failed to close response body")
+		}
+	}(response.Body)
+
+	res := &[]models.Book{}
+	err = json.NewDecoder(response.Body).Decode(res)
+	if err != nil {
+		fmt.Println("Failed to decode the json received from an internal service")
+		err = errors.New("Internal decoding error")
+		return nil, err
+	}
+	return res, nil
+}
+
+func (gc *GatewayConnector) GetRecommendations(books *[]models.Book, prefs *models.PreferencesList) (*[]models.Book, error) {
+	url := fmt.Sprintf(gc.config.CatalogueAddress + gc.config.ApiPath)
+	booksdata, err := json.Marshal(books)
+	if err != nil {
+		fmt.Println("Failed to encode input data")
+		return nil, err
+	}
+	prefsdata, err := json.Marshal(prefs)
+	if err != nil {
+		fmt.Println("Failed to encode input data")
+		return nil, err
+	}
+	request, err := http.NewRequest("GET", url, bytes.NewReader(append(booksdata, prefsdata...)))
+	if err != nil {
+		fmt.Println("Failed to create an http request")
+		return nil, err
+	}
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("Failed to get comics from internal service")
+		return nil, err
+	}
+
+	var res *[]models.Book
+	err = json.NewDecoder(response.Body).Decode(res)
+	if err != nil {
+		fmt.Println("Failed to decode data from internal service")
+		err = errors.New("Decoding error")
+		return nil, err
+	}
+
+	return res, err
 }
--- src/gateway/connector/connector_interface.go
+++ src/gateway/connector/connector_interface.go
@@ -4,4 +4,6 @@ import "RSOI/src/gateway/models"
 
 type IGatewayConnector interface {
 	GetUserPreferences(userUuid string) (*models.PreferencesList, error)
+	GetCatalogue() (*[]models.Book, error)
+	GetRecommendations(books *[]models.Book, prefs *models.PreferencesList) (*[]models.Book, error)
 }
