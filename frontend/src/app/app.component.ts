6cbfc539e48aa5791f57ed001f4c8962ac43e90b
Dmintriy Ivanov <shooterbot@gmail.com>
2022-09-16 14:12:27 +0300

    Made the icon shown in the topbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from "./models/user";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  public user: User;
+  constructor() {
+    this.user = null;
+  }
 }
