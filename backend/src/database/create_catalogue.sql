e1d3e2803541c2352867a44a6bea4652c0a3e521
Dmintriy Ivanov <shooterbot@gmail.com>
2022-09-17 03:43:08 +0300

    Added db creation script

---
+++ backend/src/database/create_catalogue.sql
@@ -0,0 +1,30 @@
+drop table if exists books;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+create table if not exists books
+(
+	id serial primary key,
+	uid uuid unique default uuid_generate_v4(),
+	name varchar(50),	
+	publisher varchar(50),
+	year int,
+	rating int,
+	status bool
+);
+
+drop table if exists books_tags;
+create table if not exists books_tags
+(
+	book_id int,
+	tag varchar(20)
+);
+
+insert into books(id, name, publisher, year, rating, status) values(1, 'Город Грехов', 'Publisher1', 1991, 5, true);
+insert into books_tags(book_id, tag) values(1, 'Боевик');
+insert into books(id, name, publisher, year, rating, status) values(2, 'Cкотт Пилигрим', 'Publisher2', 2004, 5, true);
+insert into books_tags(book_id, tag) values(2, 'Юмор');
+insert into books(id, name, publisher, year, rating, status) values(3, 'Танкистка', 'Publisher3', 1988, 4, true);
+insert into books_tags(book_id, tag) values(3, 'Фантастика');
+insert into books(id, name, publisher, year, rating, status) values(4, 'Аннарасуманара', 'Publisher4', 2010, 5, true);
+insert into books_tags(book_id, tag) values(4, 'Детектив');
+insert into books(id, name, publisher, year, rating, status) values(5, 'Всеведущий читатель', 'Publisher5', 2004, 5, false);
+insert into books_tags(book_id, tag) values(5, 'Магия');
